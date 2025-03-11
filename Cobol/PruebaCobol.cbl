c6b05db54cc0cf7f0efa8d6eace52200e56f2486
Julian <ludicolo2209@gmail.com>
2025-03-10 21:48:01 -0500

    Primeras Variables realeas

--- Cobol/PruebaCobol.cbl
+++ Cobol/PruebaCobol.cbl
@@ -24,12 +24,24 @@
        FILE SECTION.
 
        WORKING-STORAGE SECTION.
-           01 NUMERO1 PIC 999 VALUE 15.
+           01 Numero1 PIC 99 VALUE 15.
+           01 Numero2 PIC 99 VALUE 15.
+           01 Resultado PIC 99 VALUE 0.
+           01 CIEN-CONSTANTE CONSTANT AS 100.
+           01 Names PIC A(6) VALUE "Julian".
+           01 Numero3 PIC 999V99 VALUE 11,11.
+           01 NumeroNegativo PIC S99 VALUE -98.
+           01 TestDecimal PIC S9(2)V9(3) VALUE -58,311.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-           DISPLAY "HOLA" ESPACIO "COBOL".
-           DISPLAY ANGULAR-DERECHA "HOLA BUENAS TARDES"
-           ANGULAR-IZQUIERDA.
-           DISPLAY NUMERO1.
-            STOP RUN.
+           COMPUTE Resultado = Numero1 + Numero2.
+
+       MOSTRAR-RESULTADO.
+           DISPLAY Resultado.
+           DISPLAY "MUESTRO UN LITERAL".
+           DISPLAY CIEN-CONSTANTE.
+           DISPLAY Numero3.
+           DISPLAY NumeroNegativo.
+           DISPLAY TestDecimal.
+           STOP RUN.
        END PROGRAM LEARN-COBOL.
